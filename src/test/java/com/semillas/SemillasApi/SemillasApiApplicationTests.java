ae13b8d90c469d3045f04a46f99e225696690e53
RaquelMontero <raquelmonterot98129@gmail.com>
2021-11-11 14:11:33 -0400

    Init Semillas Project

---
+++ src/test/java/com/semillas/SemillasApi/SemillasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semillas.SemillasApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemillasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
